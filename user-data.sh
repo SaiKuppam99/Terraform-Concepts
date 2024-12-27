cb1fac77a0129ceadb96300aed9d8e4433f14227
SaiKuppam99 <149535931+SaiKuppam99@users.noreply.github.com>
2024-12-27 02:53:07 +0000

    committing user-data

---
+++ user-data.sh
@@ -0,0 +1,10 @@
+# #!/bin/bash
+# sudo apt update
+# sudo apt install nginx -y
+# sudo apt install git -y
+# sudo git clone https://github.com/SaiKuppam99/web-page.git
+# sudo rm -rf /var/www/hmtl/index.nginx-debian.html
+# sudo cp webpage.index.html /var/www/html/index.html
+# echo "<h1>${var.vpc_name}-public-server-${count.index + 1}</h1>" >> /var/www/html/index.html
+# sudo systemctl start nginx
+# sudo systemctl enable nginx
