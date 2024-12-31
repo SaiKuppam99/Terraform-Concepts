be50e5b25387aac7fedadadc74e4b6db0e5f8a67
SaiKuppam99 <149535931+SaiKuppam99@users.noreply.github.com>
2024-12-31 16:57:12 +0000

    updated the user data

--- user-data.sh
+++ user-data.sh
@@ -11,7 +11,7 @@ sudo rm -f /var/www/html/index.nginx-debian.html
 sudo cp /tmp/web-page/index.html /var/www/html/index.html
 
 # Add custom content without overwriting the original design
-sudo sed -i '1i <h1>${vpc_name}-public-server-${count.index + 1}</h1>' /var/www/html/index.html
+sudo sed -i '1i <h1>${var.vpc_name}-public-server-${count.index + 1}</h1>' /var/www/html/index.html
 sudo sed -i '2i <h1>This means the upgrade worked well</h1>' /var/www/html/index.html
 
 # Start and enable nginx
